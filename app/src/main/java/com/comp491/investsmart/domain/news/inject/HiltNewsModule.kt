54037c51dcf75ddfa340c2d2b8e5f5f4d767db94
damlayildiz <damlayildiz19@ku.edu.tr>
2022-11-24 18:26:09 +0200

    add new data/domain modules

---
+++ app/src/main/java/com/comp491/investsmart/domain/news/inject/HiltNewsModule.kt
@@ -0,0 +1,10 @@
+package com.comp491.investsmart.domain.news.inject
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class HiltNewsModule {
+}
