50a943ea558fa227c24b903388f0c0aa18714322
damlayildiz <damlayildiz19@ku.edu.tr>
2022-12-28 00:24:36 +0200

    feat: wire comments screen

--- app/src/main/java/com/comp491/investsmart/ui/comments/CommentsViewModel.kt
+++ app/src/main/java/com/comp491/investsmart/ui/comments/CommentsViewModel.kt
@@ -1,43 +1,56 @@
 package com.comp491.investsmart.ui.comments
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.comp491.investsmart.data.api.retrofit.InvestSmartService
+import androidx.lifecycle.viewModelScope
 import com.comp491.investsmart.domain.comments.entities.Comment
+import com.comp491.investsmart.domain.comments.usecases.AddCommentUseCase
+import com.comp491.investsmart.domain.comments.usecases.GetAssetCommentsUseCase
+import com.comp491.investsmart.navigation.NavRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 data class CommentsVMState(
     val comments: List<Comment>,
+    val isLoading: Boolean,
 )
 
 @HiltViewModel
 class CommentsViewModel @Inject constructor(
-    private val investSmartService: InvestSmartService,
+    savedStateHandle: SavedStateHandle,
+    private val getAssetCommentsUseCase: GetAssetCommentsUseCase,
+    private val addCommentUseCase: AddCommentUseCase,
 ) : ViewModel() {
 
     private val vmState = CommentsVMState(
-        comments = List(20) {
-            Comment(
-                id = 0,
-                username = "golden",
-                userId = 12,
-                assetTicker = "PHID",
-                text = "If you never walked at least once, it was a road ride.",
-                date = "23.06.2021",
-                likeCount = 10,
-                importedFrom = "",
-                answerCount = 5,
-            )
-        },
+        comments = emptyList(),
+        isLoading = true,
     )
     private val _vmState = MutableStateFlow(vmState)
     val uiState: StateFlow<CommentsVMState> = _vmState.asStateFlow()
+    val commentId = savedStateHandle.get<String>(NavRoute.Comments.commentId)?.toString()
 
     init {
+        commentId?.let {
+            viewModelScope.launch {
+                // TODO: Handle error
+                val comments: List<Comment> =
+                    getAssetCommentsUseCase(
+                        assetTicker = null,
+                        commentParent = commentId,
+                        userId = null,
+                    ).data ?: emptyList()
 
+                _vmState.value = CommentsVMState(
+                    comments = comments,
+                    isLoading = false,
+                )
+            }
+        }
     }
 
     fun onLikeButtonClicked(commentId: Int) {
@@ -49,6 +62,12 @@ class CommentsViewModel @Inject constructor(
     }
 
     fun onReplyClicked(text: String) {
-
+        viewModelScope.launch {
+            addCommentUseCase(
+                assetTicker = vmState.comments[0].assetTicker,
+                text = text,
+                parentId = commentId?.toInt(),
+            )
+        }
     }
 }
